84c43207cc2cdb3265cf072fd06edab682bab9ff
Takuya Ito <ito0804takuya@gmail.com>
2024-04-30 21:25:15 +0900

    main関数実装

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## migrate実行
 GO_ENV=dev go run migrate/migrate.go
+
+## サーバ起動
+GO_ENV=dev go run main.go
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repositoty"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+)
 
 func main() {
-	fmt.Println("hoge")
+	// DB
+	db := db.NewDB()
+
+	// ユーザ関連 ---------------------------------
+	// レポジトリ
+	userRepository := repositoty.NewUserRepository(db)
+	// ユースケース
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	// コントローラー
+	userController := controller.NewUserController(userUsecase)
+
+	// ルーター
+	e := router.NewRouter(userController)
+
+	// サーバ起動
+	e.Logger.Fatal(e.Start(":8080"))
 }
